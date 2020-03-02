7305b831427423e66d52bf17fb98dfb0b2ba51a8
André Luiz Cardoso <andrecardoso@gmail.com>
2020-03-02 12:04:00 -0300

    Schedule initial implementation

--- src/schedule.js
+++ src/schedule.js
@@ -1,6 +1,25 @@
+/* eslint-disable newline-after-var */
+
+class TransitionHandler {
+
+  transition(from, to) {
+    if (from && from.style) {
+      from.style.display = "none";
+    }
+
+    if (to && to.style) {
+      to.style.display = "block";
+    }
+  }
+
+}
+
 export default class Schedule {
-  constructor() {
+  constructor(transitionHandler = new TransitionHandler()) {
+    this.transitionHandler = transitionHandler;
     this.items = [];
+    this.playingItem = null;
+    this.itemDurationTimer = null;
   }
 
   start() {
@@ -9,10 +28,25 @@ export default class Schedule {
   }
 
   stop() {
-
+    clearTimeout(this.itemDurationTimer);
+    this.playingItem = null;
   }
 
   play() {
+    clearTimeout(this.itemDurationTimer);
 
+    let nextItem = this.items.shift();
+    this.items.push(nextItem);
+
+    let previousItem = this.playingItem;
+    this.playingItem = nextItem;
+
+    if (!previousItem || previousItem !== nextItem) {
+      const previousElement = previousItem ? previousItem.element : null;
+
+      this.transitionHandler.transition(previousElement, nextItem.element);
+    }
+
+    this.itemDurationTimer= setTimeout(() => this.play(), nextItem.duration * 1000);
   }
 }
