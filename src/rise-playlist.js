58683b4b0fbde06ad25531587790a18ebdc58cfb
Alex Deaconu <alex.deaconu@gmail.com>
2021-04-28 15:13:59 -0400

    Set play-until-done and element properties via Polymer method (#50)
    * Set the play-until-done attribute on the element (#51)
    components require the attribute to know
    if they should send done or not
    
    * Set element properties via polymer method (#52)
    Allows object type properties to be set correctly
    Convert property names to camel case
    
    Send start event after properties are updated

--- src/rise-playlist.js
+++ src/rise-playlist.js
@@ -63,8 +63,6 @@ export class RisePlaylistItem extends RiseElement {
 
       this.firstElementChild.addEventListener("rise-components-ready", () => this._setReady());
       this.firstElementChild.addEventListener("rise-components-error", () => this._isError = true);
-
-      RisePlayerConfiguration.Helpers.sendStartEvent( this.firstElementChild );
     }
   }
 
@@ -226,6 +224,22 @@ export default class RisePlaylist extends RiseElement {
     }
   }
 
+  _convertHyphensToCamelCase (string) {
+    return string.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
+  }
+
+  _setPropertiesNative( element, attributes ) {
+    const updatedAttributes = {};
+
+    Object.entries(attributes).forEach(([key, value]) => {
+      updatedAttributes[this._convertHyphensToCamelCase(key)] = value;
+    });
+
+    console.log(`Setting attributes component=${element.tagName.toLowerCase()}, id=${element.id} to value`, updatedAttributes);
+
+    element.setProperties(updatedAttributes);
+  }
+
   _itemsChanged(items) {
     this._removeAllItems();
 
@@ -260,9 +274,13 @@ export default class RisePlaylist extends RiseElement {
 
       element.setAttribute("id", playListItemId + "_" + item.element.tagName);
 
-      Object.entries(item.element.attributes).forEach(([key, value]) => {
-        element.setAttribute(key, value);
-      });
+      if (item["play-until-done"]) {
+        element.setAttribute("play-until-done", item["play-until-done"]);
+      }
+
+      RisePlayerConfiguration.Helpers.getComponentAsync( element )
+        .then( this._setPropertiesNative.bind( this, element, item.element.attributes ))
+        .then( RisePlayerConfiguration.Helpers.sendStartEvent.bind( null, element ));
 
       playListItem.appendChild(element);
 
